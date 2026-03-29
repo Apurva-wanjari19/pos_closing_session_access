9f4da3bd8284b8287ad6fd002c87f97f2dba7061
Apurva-wanjari19 <apurvawanjari175@gmail.com>
2026-03-29 15:49:18 +0530

    Add files via upload

---
+++ pos_closing_session_access/static/src/js/close_session_restriction.js
@@ -0,0 +1,37 @@
+/** @odoo-module **/
+
+import { patch } from "@web/core/utils/patch";
+import { ClosePosPopup } from "@point_of_sale/app/components/popups/closing_popup/closing_popup";
+import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+import { _t } from "@web/core/l10n/translation";
+
+patch(ClosePosPopup.prototype, {
+
+    async confirm() {
+
+        // console.log("Close session confirm triggered");
+
+        const userId = this.pos.user.id;
+
+        const result = await this.pos.data.call(
+            "res.users",
+            "read",
+            [[userId], ["pos_close_session_access"]]
+        );
+
+        const hasAccess = result?.[0]?.pos_close_session_access;
+
+        // console.log("User access from backend:", hasAccess);
+
+        if (!hasAccess) {
+            this.dialog.add(AlertDialog, {
+                title: _t("Access Denied"),
+                body: _t("Contact your admin for closing POS session access."),
+            });
+            return;
+        }
+
+        return super.confirm(...arguments);
+    },
+
+});
